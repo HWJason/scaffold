772a1af136a88f63cbc72498be457e304951867a
jason <2920569613@qq.com>
2019-11-01 11:46:38 +0800

    第二次更新

--- common/add.js
+++ common/add.js
@@ -13,6 +13,7 @@ module.exports = () => {
    let gitUrl = yield prompt('Git https link: ')
    let branch = yield prompt('Branch: ')
    console.log('用来测试测试上传')
+   console.log('用来测试测试上传1')
     
    // 避免重复添加
    if (!config.tpl[tplName]) {
